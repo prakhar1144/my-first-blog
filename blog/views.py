3f2451013c59645bd404cf93edc5c30cea9db589
prakhar1144 <prakhar841301@gmail.com>
2020-08-31 19:14:22 +0530

    image field added, mediaroot, mediaurl created, url.py updated for this, image added as field in form.py, post_new and post_edit function in view.py updated with request.FILES to take image file and save it, image width set to 75% of division

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -29,7 +29,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -38,7 +38,7 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
