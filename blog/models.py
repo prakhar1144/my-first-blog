d771e911404b3de8a6d09ecc5ea4c996f987a4a0
prakhar1144 <prakhar841301@gmail.com>
2020-09-01 13:17:18 +0530

    else condition added if title image is not available

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from ckeditor.fields import RichTextField
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = RichTextField(blank=True, null= True)
+    text = RichTextField(blank=True, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(null=True, blank=True)
